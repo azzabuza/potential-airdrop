c5e051372c906478345170e8dd4280fe16b8e5ae
Azza <60853582+azzabuza@users.noreply.github.com>
2024-10-09 15:00:34 +0700

    frist commit

---
+++ timeline.js
@@ -0,0 +1,25 @@
+function fetchAndDisplayTimeline() {
+fetch('timeline.json')
+.then(response => response.json())
+.then(data => {
+const timelineBox = document.getElementById('timeline-box');
+let timelineHTML = '';
+
+data.forEach(item => {
+timelineHTML += `
+<div class="timeline-tile">
+<h3>${item.title}</h3>
+<p style="margin:0;font-weight:500;color:#2e8b57;">${item.developer}</p>
+<p style="font-size:13px;">${item.platform}</p>
+<a href="${item.link}" title="${item.title}">JOIN AIRDROP</a>
+<span>${item.date}</span>
+</div>
+`;
+});
+
+timelineBox.innerHTML = timelineHTML;
+})
+.catch(error => console.error('Error fetching timeline data:', error));
+}
+
+fetchAndDisplayTimeline();
